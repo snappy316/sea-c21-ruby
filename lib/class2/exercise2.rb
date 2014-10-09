e6374d8e601bfd2ba0c22ef79053088491a6aa78
David Ker <snappy316@gmail.com>
2014-10-08 20:52:56 -0700

    Completed exercise 1, 2, 3

--- lib/class2/exercise2.rb
+++ lib/class2/exercise2.rb
@@ -3,3 +3,5 @@
 # Assuming leap-years don't exist, display the minutes in a decade.
 #
 # TIP: I expect you to use a mathematical equation.
+
+puts 10 * 365 * 24 * 60
--- lib/class2/exercise3.rb
+++ lib/class2/exercise3.rb
@@ -2,3 +2,5 @@
 #
 # If a person is 1,160 million seconds old, display their age in years. As
 # always, assume leap years don't exist.
+
+puts 1_160_000_000 / 60 / 60 / 24 / 365
